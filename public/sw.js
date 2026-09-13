1144e8463eeb60165ccb5e966c01b64d5057df6d
wpguidexpro-hub <wpguidexpro@gmail.com>
2026-09-13 14:43:22 +0530

    feat: add DepthCut offline service worker

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+const CACHE='depthcut-v1';
+const APP=['/','/index.html','/manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('/'))))});
